86923f6b4055f0f4675348d8de1902889f5d9c1d
Dorukan Eskiçeri <dorukaneskiceri@gmail.com>
2022-10-07 02:05:50 +0300

    first composables

--- app/src/main/java/com/example/composexample/MainActivity.kt
+++ app/src/main/java/com/example/composexample/MainActivity.kt
@@ -3,41 +3,46 @@ package com.example.composexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composexample.ui.theme.ComposeXampleTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeXampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            CustomPreview()
         }
     }
 }
 
+@Preview(widthDp = 100, heightDp = 100, showBackground = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeXampleTheme {
-        Greeting("Android")
+fun CustomPreview() {
+    Column {
+        Text(
+            text = "Hello World",
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.Black)
+                .size(50.dp),
+            color = Color.Red
+        )
+        Text(
+            text = "Hello World",
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.White)
+                .size(50.dp)
+        )
     }
 }
